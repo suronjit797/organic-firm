a264351aeb100269d88fd46ca47f63c1c33c974a
Suronjit pal <suronjit797@gmail.com>
2022-04-04 18:44:36 +0600

    review and rating

--- src/page/Home/Home.js
+++ src/page/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom'
 
 import './Home.css'
@@ -11,7 +11,37 @@ const Home = () => {
     // dynamic title
     document.title = 'Organic Farm || Home'
 
-    const [reviews] = useReview()
+    const [reviews, setReviews] = useReview()
+
+    const [userName, setUserName] = useState('')
+    const [userReview, setuserReview] = useState('')
+    const [userRating, setuserRating] = useState(0)
+
+    const reviewHandler = event => {
+        event.preventDefault()
+        let id = new Date().getTime()
+
+        const ratingObject = {
+            id,
+            name: userName,
+            review: userReview,
+            ratings: userRating,
+            picture: "images/user1.png"
+        }
+        setReviews([ratingObject, ...reviews])
+
+        console.log(reviews)
+    }
+
+    const ratingHandler = e => {
+        let rating = e.target.value
+        if (rating > 5){
+            rating = 5
+        }else if( rating < 0){
+            rating = 0
+        }
+            setuserRating(rating)
+    }
 
 
     return (
@@ -34,7 +64,7 @@ const Home = () => {
                     <Link to="/buy-now" className='primary_button mt-3'> <span>Buy now</span> </Link>
                 </div>
             </div>
-            <hr className='my-5'/>
+            <hr className='my-5' />
 
             <div className="home_review mt-5">
                 <h1 className="text-center fw-bold mb-4"> Cutomer Review ({reviews.length}) </h1>
@@ -50,12 +80,49 @@ const Home = () => {
                             ))
                         )
                     }
+                    <p className="text-center">
+                        {
+                            reviews.length > 3 ?
+                                <Link to='/review' className='mt-4 primary_button'> <span>See more</span> </Link>
+                                : ''
+                        }
+                    </p>
 
-                    {
-                        reviews.length > 3 ? <p className="text-center">
-                            <Link to='/review' className='mt-4 primary_button'> <span>See more</span> </Link>
-                        </p> : ''
-                    }
+                    <form onSubmit={reviewHandler}>
+                        <input
+                            type="text"
+                            name="name"
+                            id="name"
+                            placeholder='Name'
+                            className="form-control mb-3"
+                            value={userName}
+                            onChange={e => setUserName(e.target.value)}
+                        />
+                        <input
+                            type="number"
+                            name="rating"
+                            id="rating"
+                            placeholder='rating 1-5'
+                            className="form-control mb-3"
+                            value={userRating}
+                            onChange={ratingHandler}
+                        />
+                        <textarea
+                            placeholder='Provide Your Message'
+                            name="reviewForm"
+                            className='form-control mb-3'
+                            id="reviewForm"
+                            rows="5"
+                            value={userReview}
+                            onChange={e => setuserReview(e.target.value)}
+                        ></textarea>
+
+                        <button 
+                        type="submit" 
+                        className='mx-auto d-block primary_button border-0' 
+                        disabled={!userName}
+                        > <span>Submit</span></button>
+                    </form>
                 </div>
             </div>
 
